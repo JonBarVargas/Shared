48100be1b0b3fadcd3176dbbc9df52462714885a
JonBarVargas <jonathanbvargas@hotmail.com>
2021-11-02 06:47:03 -0600

    Finished story selection v1

---
+++ Shared-app/Shared/Assets/Scripts/StoryOne.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StoryOne : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    public AudioSource source;
+    public AudioClip clip;
+    void Start()
+    {
+        source.PlayOneShot(clip);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
